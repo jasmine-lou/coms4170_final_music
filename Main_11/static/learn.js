bc5272ef591f2cc2c59b676cbe1ce3e9072a46df
Joey Hou <77761795+joeyhou0804@users.noreply.github.com>
2022-04-19 00:32:07 -0400

    Add files via upload
    UI Changed

--- Main_11/static/learn.js
+++ Main_11/static/learn.js
@@ -43,9 +43,9 @@ function display_headers(data){
 }
 
 function display_remaining_data(data){
-    $("#ui_here").append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
+    /*$("#ui_here").append("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
         "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi " +
         "ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum " +
         "dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia " +
-        "deserunt mollit anim id est laborum.")
+        "deserunt mollit anim id est laborum.")*/
 }
