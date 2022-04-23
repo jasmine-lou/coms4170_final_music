b1ade0d324969b716dd55d0304c56b30a622fe3c
Jasmine Lou <65261599+jasmine-lou@users.noreply.github.com>
2022-04-23 16:16:01 -0400

    Update learn.js

--- Main_11/static/learn.js
+++ Main_11/static/learn.js
@@ -2,9 +2,16 @@ $(document).ready(function(){
     display_headers(data)
     display_remaining_data(data)
 
-        if(data['id'] == max && data['type'] == 'scales'){
+    $("#display_page_number").text("Page " + data['id'] + '/' + max)
+
+    if(data['id'] == max && data['type'] == 'scales'){
         $("#next_button").empty()
         $("#next_button").text("Go to Quiz!")
+    }
+
+    if(data['id'] == 1 && data['type'] == 'scales'){
+        $("#prev_button").empty()
+        $("#prev_button").text("Return to Keys!")
     }
 
     if(data['id'] == max && data['type'] == 'keys'){
@@ -25,13 +32,18 @@ $(document).ready(function(){
             }
         }
     })
-
+    
     $("#prev_button").click(function () {
         if(data['id'] > 1){
             window.location.replace('http://127.0.0.1:5000/' + String(data['type']) + '/' + String(data['id'] - 1))
         }
         else{
-            alert('this is the first item')
+            if (data['type'] == 'scales'){
+                window.location.replace('http://127.0.0.1:5000/keys/4')
+            }
+            else {
+                alert('this is the first item')
+            }
         }
     })
 
