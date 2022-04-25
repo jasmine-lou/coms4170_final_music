31946579e73cd58db8e235099bdd3a474420643a
CHONGZHI XU <90977332+ChongzhiXu@users.noreply.github.com>
2022-04-25 15:34:32 -0400

    Update results.js

--- Main_11/static/results.js
+++ Main_11/static/results.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-    $("#results").empty();
-    $("#results").append("<div class='row'>You Got " + num_correct + " correct answers out of 4, Congratulations!</div>")
     $("#buttons").append(`<a class="btn btn-primary" href="/home" role="button">Homepage</a>`)
-    $("#buttons").append(`<a class="btn btn-primary" href="/quiz/1" role="button">Quiz Again</a>`)
+    $("#buttons").append(`<a class="btn btn-primary" href="/quiz" role="button">Quiz Again</a>`)
 })
