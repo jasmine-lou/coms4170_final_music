90a1eadb1672182215b514b015b985b85d08fbbb
CHONGZHI XU <90977332+ChongzhiXu@users.noreply.github.com>
2022-04-28 16:28:41 -0400

    Delete the buttons

--- Main_11/static/results.js
+++ Main_11/static/results.js
@@ -1,4 +1,3 @@
 $(document).ready(function() {
-    $("#buttons").append(`<a class="btn btn-primary" href="/home" role="button">Homepage</a>`)
-    $("#buttons").append(`<a class="btn btn-primary" href="/quiz" role="button">Quiz Again</a>`)
+    
 })
